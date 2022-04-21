523c88e0890b8d7c43aeee5e6623e0660bda21b1
malvarezg1 <m.alvarezg1@uniandes.edu.co>
2022-04-21 16:15:25 -0500

    bla

--- marvelapp/src/components/marvel.js
+++ marvelapp/src/components/marvel.js
@@ -2,20 +2,36 @@ import React, { useState, useEffect } from "react";
 
 function Marvel(){
 
-    const [joke, setJoke] = useState("");
+    const [joke, setJoke] = useState([]);
 
     useEffect(()=>{
-
-            const URL = "https://api.chucknorris.io/jokes/random";
+        if(!navigator.onLine){
+            if(localStorage.getItem("joke") === null) {
+                console.log("Sin conexión y sin cache")
+            } else {
+                console.log("Sin conexión y con cache")
+                setJoke(localStorage.getItem("joke"));
+            }
+        } else {
+            console.log("Con conexión")
+            const URL = "https://gateway.marvel.com:443/v1/public/characters?ts=173534988375&apikey=108b2e2548eb4b8d7e638116256aa10f&hash=f110989889cec9ccf39d344380e8130b";
             fetch(URL).then(res=>res.json()).then(res=>{
-                console.log(res.value);
-                setJoke(res.value);
+                console.log(res.data.results[0].thumbnail.path)
+                localStorage.setItem("joke", res.data.results);
+                setJoke(res.data.results);
             })
-        
+        }
     }, []);
 
     return(
-        <h1>{joke}</h1>
+        <div>
+        {joke.map(hero => (
+            <div>
+                <img src={hero.thumbnail.path} alt={hero.name}></img>
+                <p>{hero.name}</p>
+            </div>
+        ))}
+        </div>
     )
 }
 
